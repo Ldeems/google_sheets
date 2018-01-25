dff80dabce9b5ea6ab554ba4d1805507af4f08db
Ldeems <luke.deems@yahoo.com>
2018-01-25 16:53:37 -0500

    wow finaly got thought he quick start and pulled the dummy sheet in the quick start back

--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 require "sinatra"
-require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'fileutils'
+
 load './local_ENV.rb' if File.exist?('./local_ENV.rb')
 
 
 
-# get "/" do
-#     erb :page_one
-# end
+get "/" do
+    erb :page_one
+end
